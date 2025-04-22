99c2e688c7555e95fea06e6c1be9137eabc95e08
ikenna nwaka <inwaka39@gmail.com>
2025-04-22 18:59:40 +0200

    paperBtn

--- script.js
+++ script.js
@@ -59,3 +59,4 @@ function resetGame() {
 resetGameBtn.addEventListener("click", resetGame);
 
 const rockBtn = document.getElementById("rock-btn");
+const paperBtn = document.getElementById("paper-btn");
