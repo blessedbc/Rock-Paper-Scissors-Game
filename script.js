9253b55171b7a7dc54a89fcb0f45f5e0ac81dd47
ikenna nwaka <inwaka39@gmail.com>
2025-04-22 19:00:15 +0200

    scissorsBtn

--- script.js
+++ script.js
@@ -60,3 +60,4 @@ resetGameBtn.addEventListener("click", resetGame);
 
 const rockBtn = document.getElementById("rock-btn");
 const paperBtn = document.getElementById("paper-btn");
+const scissorsBtn = document.getElementById("scissors-btn");
